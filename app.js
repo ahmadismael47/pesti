bc7bafad66734c035c1d28df2ce966339242575a
ahmadismael47 <ismaelahmad63@hotmail.com>
2022-09-16 17:29:10 +1200

    added user auth0

--- app.js
+++ app.js
@@ -2,15 +2,39 @@ const express = require("express")
 const app = express()
 const products = require("./backend/data/product")
 const cors = require("cors")
+const dotenv = require("dotenv")
+const { auth, requiresAuth } = require("express-openid-connect")
+let user = []
+dotenv.config()
+
+app.use(
+  auth({
+    authRequired: false,
+    auth0Logout: true,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+    secret: process.env.SECRET,
+  })
+)
+
 app.use(express.static("./frontend"))
 
-// app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 
 app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.send("api")
+// app.get("/", (req, res) => {
+//   res.send("api")
+// })
+// app.get("/", (req, res) => {
+//   res.json(req.oidc.isAuthenticated() ? "Logged in" : "Logged out")
+// })
+
+app.get("/profile", requiresAuth(), (req, res) => {
+  // res.json(req.oidc.user)
+  user = req.oidc.user
+  res.json({ user })
 })
 
 app.get("/api/products", (req, res) => {
@@ -29,6 +53,6 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product)
 })
 
-const PORT = 5500
+const PORT = 3000
 
-app.listen(PORT, console.log("Server is running on port 5000"))
+app.listen(PORT, console.log("Server is running on port 3000"))
--- frontend/script/components/navbar.js
+++ frontend/script/components/navbar.js
@@ -4,16 +4,56 @@ export const rendNavbar = () => {
   const linkContainer = document.querySelector(".navbar")
   const link = document.createElement("ul")
   const sidebar = document.querySelector(".hamburger")
+
+  let user = []
+
   link.innerHTML = navLinks
     .map((item) => {
       const { name, path, icon, id } = item
-      return ` <li><i class="${icon}"></i><a  class="${name}" href="${path}">${name}</a></li>`
+      return ` <li  class="${name}" >
+                  <i class="${icon}"></i><a href="${path}">${name}
+                  </a>
+
+             </li>       
+         
+             `
     })
     .join("")
 
   link.classList.add("links")
   linkContainer.append(link)
 
+  const checkUser = async () => {
+    try {
+      const { data } = await axios.get("/profile")
+      user = data
+      console.log(user.user)
+      const login = document.createElement("li")
+      const logout = document.createElement("li")
+      login.innerHTML = ` 
+                          <a>
+                          <i class="fa-solid fa-id-badge"></i><a  class="${user.user.name}">${user.user.name}
+                          <img src=${user.user.picture} />
+                          </a>
+  
+                        `
+      logout.innerHTML = ` 
+                        <a>
+                        <i class="fa-solid fa-user-minus"></i><a  class="logout" href="/logout">Logout
+                        </a>
+
+                      `
+      document.querySelector(".Login").remove()
+
+      link.append(login)
+      link.append(logout)
+      // linkContainer.appendChild(login)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  checkUser()
   //////////SIDEBAR
 
   sidebar.addEventListener("click", function () {
