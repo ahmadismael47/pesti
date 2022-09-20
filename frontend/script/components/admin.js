fcf9355464b4f295da9def437fbca7a65bb18797
ahmadismael47 <110211694+ahmadismael47@users.noreply.github.com>
2022-09-20 11:57:36 +0100

    Fetch products / admin dashboard

--- frontend/script/components/admin.js
+++ frontend/script/components/admin.js
@@ -2,15 +2,18 @@ let pName = document.querySelector(".prod_name")
 let pPrice = document.querySelector(".prod_price")
 let pImg = document.querySelector(".prod_img")
 let pDesc = document.querySelector(".prod_desc")
+let pBrand = document.querySelector(".prod_brand")
+let pCategory = document.querySelector(".prod_category")
 let add = document.querySelector(".add")
 const addProduct =async (e) =>{
-    e.preventDefault()
     try {
     //     let {data } = await axios.post("/api/products")
     //     let dataa = {}
         pName = pName.value
         pPrice = pPrice.value
         pImg = pImg.value
+        pCategory = pCategory.value
+        pBrand = pBrand.value
         pDesc = pDesc.value
     //     dataa = {pName, pPrice,pImg, pDesc}
     //    data = dataa
@@ -23,7 +26,9 @@ const addProduct =async (e) =>{
           name:pName,
           price:pPrice,
           image:pImg,
-          description: pDesc
+          description: pDesc,
+          category:pCategory,
+          brand:pBrand,
         
 
         }
@@ -60,6 +65,75 @@ const fetchProducts = async () => {
 fetchProducts()
 
 
+// const productsCont = document.querySelector(".prod_list_admin")
+// const prodItem = document.createElement("tr")
+var productsList = []
+
+const asdqwe = async () => {
+    try {
+      const { data } = await axios.get("/api/products")
+
+      productsList = data
+      console.log(productsList)
+      renderProducts()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const productsCont = document.querySelector(".prod_list_admin")
+const prodItem = document.createElement("tbody")
+
+const renderProducts = () => {
+
+
+  prodItem.innerHTML = productsList
+    .map((item) => {
+      const {
+        _id: id,
+        name,
+        description,
+        image,
+        category,
+        price,
+        rating,
+        numReviews,
+        brand
+      } = item
+
+      
+      return `
+   
+                    
+             <tr class="${category} body">
+             
+             
+             <td>${name}</td>
+             <td>${price}</td>
+             <td>${category}</td>
+             <td>${brand}</td>
+             <td style="text-align:center"><i class="fa-solid fa-pen-to-square" style="font-size:2rem;margin-right:.8rem"></i><i class="fa-solid fa-trash trash" style="font-size:2rem"></i></td>
+             </tr>
+           
+            
+
+        `
+
+
+    })
+    .join("")
+
+
+
+
+}
+
+
+productsCont.append(prodItem)
+
+
+
+renderProducts()
 
 
 
